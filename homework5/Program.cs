108fa1ffc0f75608f033b810846dfdf5ffe2c78f
msaadat03 <saadatam@code.edu.az>
2022-06-21 00:32:22 +0300

    HomeW5

---
+++ homework5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş n -ədədinin 3 - ə və 7 - ə bölünüb - bölünməməsini tapın.
+            //int num = 21;
+            //Console.WriteLine("Enter the number:");
+
+            //if (num % 3 == 0 && num % 7 == 0)
+            //{
+            //    Console.WriteLine("Divisible by 3 and 7");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not divisible by 3 and 7");
+            //}
+            //Console.ReadLine();
+
+
+
+            //n və m ədədləri verilir.Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+            //int n = 1;
+            //int m = 3;
+            //Console.WriteLine("number");
+            //if (n % 2 == 0 && m % 2 == 0)
+            //{
+            //    Console.WriteLine(n + m);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("odd numbers");
+            //}
+            //Console.ReadLine();
+
+
+
+            //Verilmis n ve m(n<m) ededleri arasindaki tek ededlerin cemini tapin.
+            //int n = 3;
+            //int m = 5;
+            //int sum = 0;
+            //if (n < m)
+            //{
+            //    for (int i = n + 1; i <= m; i++);
+            //    {
+            //       if (i % 2 == 1)
+            //            sum += i;
+            //    }
+            //    Console.WriteLine(sum);
+            //}
+
+
+
+            //Verilmish arrayin icindeki tek ededlerin cemini tapin
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int odd = 0;
+
+            //for (int i = 0; i < arr.Length; i++) ;
+            //{
+            //    if (i % 2 == 1)
+            //        odd += arr[i];
+            //}
+            //Console.WriteLine(odd);
+
+
+
+
+            //Verilmish arrayin icindeki cut ededlerin sayini tapin.
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int even = 0;
+
+            //for (int i = 0; i < arr.Length; i++) ;
+            //{
+            //    if (i % 2 == 0)
+            //        even++;
+            //}
+            //Console.WriteLine(even);
+
+        }
+    }
+}
